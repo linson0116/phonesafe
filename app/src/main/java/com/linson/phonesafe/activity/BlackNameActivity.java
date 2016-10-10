2ed8c5558cc702381104082e86add706b5652fcc
linson0116 <linson0116@163.com>
2016-10-10 22:57:55 +0800

    20161010 2257

--- app/src/main/java/com/linson/phonesafe/activity/BlackNameActivity.java
+++ app/src/main/java/com/linson/phonesafe/activity/BlackNameActivity.java
@@ -5,12 +5,15 @@ import android.app.AlertDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.linson.phonesafe.R;
@@ -36,6 +39,8 @@ public class BlackNameActivity extends Activity {
             super.handleMessage(msg);
         }
     };
+    private int mode;
+    private EditText et_tel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,13 +76,44 @@ public class BlackNameActivity extends Activity {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         final AlertDialog dialog = builder.create();
         View view = View.inflate(this, R.layout.dialog_blacknumber_add, null);
-        EditText et_tel = (EditText) view.findViewById(R.id.et_tel);
+        et_tel = (EditText) view.findViewById(R.id.et_tel);
         Button btn_add = (Button) view.findViewById(R.id.btn_add);
         Button btn_cancel = (Button) view.findViewById(R.id.btn_cancel);
+        final RadioGroup rg_mode = (RadioGroup) view.findViewById(R.id.rg_mode);
+        mode = 1;
+        rg_mode.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.rb_sms:
+                        mode = 1;
+                        break;
+                    case R.id.rb_phone:
+                        mode = 2;
+                        break;
+                    case R.id.rb_all:
+                        mode = 3;
+                        break;
+                }
+            }
+        });
+        final RadioButton rb_sms = (RadioButton) view.findViewById(R.id.rb_sms);
+        rb_sms.setChecked(true);
+
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String tel = et_tel.getText().toString();
+                if (!TextUtils.isEmpty(tel)) {
+                    BlackNumberDao dao = BlackNumberDao.getInstance(getApplicationContext());
+                    dao.insert(tel, "" + mode);
+                    BlackNumberInfo info = new BlackNumberInfo();
+                    info.phone = tel;
+                    info.mode = mode + "";
+                    mArrayList.add(0, info);
+                    mAdapter.notifyDataSetChanged();
+                }
+                dialog.dismiss();
             }
         });
         btn_cancel.setOnClickListener(new View.OnClickListener() {
@@ -108,10 +144,10 @@ public class BlackNameActivity extends Activity {
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             View view = View.inflate(getApplicationContext(), R.layout.black_name_item, null);
-            TextView tv_phone = (TextView) view.findViewById(R.id.tv_phone);
-            TextView tv_mode = (TextView) view.findViewById(R.id.tv_mode);
+            final TextView tv_phone = (TextView) view.findViewById(R.id.tv_phone);
+            final TextView tv_mode = (TextView) view.findViewById(R.id.tv_mode);
             tv_phone.setText(mArrayList.get(position).phone);
             String mode = mArrayList.get(position).mode;
             switch (mode) {
@@ -125,6 +161,23 @@ public class BlackNameActivity extends Activity {
                     tv_mode.setText("全部拦截");
                     break;
             }
+            Button btn_delete = (Button) view.findViewById(R.id.btn_delete);
+            btn_delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    BlackNumberDao dao = BlackNumberDao.getInstance(getApplicationContext());
+                    String phone = tv_phone.getText().toString();
+                    String mode = tv_mode.getText().toString();
+                    BlackNumberInfo info = new BlackNumberInfo();
+                    info.phone = phone;
+                    info.mode = "" + mode;
+                    dao.delete(phone);
+                    mArrayList.remove(position);
+                    mAdapter.notifyDataSetChanged();
+
+                }
+            });
+
             return view;
         }
     }
